857bfc4de5fc7f5789192261d7eedae01731d405
Aristocrat4 <galadanelia1@gmail.com>
2024-03-20 18:19:50 +0400

    add hero section with slider

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/tailwind.css";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 createApp(App).use(router).mount("#app");
