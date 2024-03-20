0e63edd2e07e54b69c5f47cc44e4580f6f13ef65
Aristocrat4 <galadanelia1@gmail.com>
2024-03-20 20:36:06 +0400

    add facts components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <Header> </Header>
     <Hero></Hero>
     <AllNews></AllNews>
+    <Facts></Facts>
   </div>
 </template>
 
@@ -11,9 +12,10 @@ import { defineComponent } from "vue";
 import Header from "@/components/Header.vue";
 import Hero from "@/components/Hero.vue";
 import AllNews from "@/components/AllNews.vue";
+import Facts from "@/components/Facts.vue";
 
 export default defineComponent({
   name: "HomeView",
-  components: { Header, Hero, AllNews },
+  components: { Header, Hero, AllNews, Facts },
 });
 </script>
